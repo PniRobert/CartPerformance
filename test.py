a8900cc3489300b7f6e2b9422459c644247baba5
Robert Li <rli@pnimedia.com>
2020-06-10 15:39:29 -0700

    back

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 locale = tz.gettz("America/Los_Angeles")
+cartUrl = "/services/printing/Cart"
 timeCol = "log_datetime"
 timeTakenCol = "time-taken"
 df = pd.read_csv("u_ex200610.log", sep="\s+", header=0,
@@ -16,10 +17,15 @@ df = pd.read_csv("u_ex200610.log", sep="\s+", header=0,
 df[timeCol] = pd.to_datetime(
     df["date"] + " " + df["time"], format="%Y%m%d %H:%M:%S", utc=True)
 
+cart = df[df["cs-uri-stem"] ==
+          cartUrl]
+total = cart[timeTakenCol].count()
 
-filt = (df["cs-uri-stem"] ==
-        "/services/printing/Cart") & (df[timeTakenCol] > 180000)
+filt = (df["cs-uri-stem"] == cartUrl
+        ) & (df[timeTakenCol] > 300000)
 x = np.vectorize(lambda t: t.astimezone(locale))(df[filt][timeCol].to_numpy())
 y = df[filt][timeTakenCol]
+longRun = y.count()
+print("{:.4%}".format(longRun/total))
 plt.plot(x, y, "go")
 plt.show()
